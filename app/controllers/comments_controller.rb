c4fcafe320f00ff6561f45dc9e2ed2b0a56a90d6
SarahB0101 <sbouchir@gmail.com>
2019-08-07 14:24:19 +0200

    second

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+	def new
+		@comment = Comment.new
+	end
+
+	def create
+		@comment = Comment.new(params[:comment])
+		@gossip = Gossip.new(title: params[:title], content: params[:content], user: User.last)
+	# p = comment_params
+ #    p[:user] = User.last
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+end
--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -20,13 +20,42 @@ class GossipsController < ApplicationController
   		flash[:success] = "Ton gossip est bien enregistré !"
   		redirect_to index_path
   	else
-  		#flash = {error: "Ton gossip n'a pas été enregistré, recommence..."}
+  		#flash[:error]= "Ton gossip n'a pas été enregistré, recommence..."
   		render new_gossip_path	
   	end
 
   end
 
+  def edit
+      @id = params[:id]
+      @gossip = Gossip.all.find(@id)
+  end
 
+
+  def update
+    @gossip = Gossip.find(params[:id])
+    if @gossip.update(title: params[:title], content: params[:content], user: User.last)
+      flash[:success] = "Ton gossip est bien modifié !"
+      redirect_to gossip_path
+     else
+     render :edit 
+    end
+    
+  end
+
+  def destroy
+     
+
+      @gossip = Gossip.all.find(params[:id])
+      @gossip.destroy
+      
+       if flash[:success1] = "Ton gossip a bien été supprimé !"
+      redirect_to index_path
+      else
+      render gossip_path
+
+      end
+  end
 
 
 end
